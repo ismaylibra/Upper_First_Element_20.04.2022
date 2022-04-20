c80da07f45ad47a5281115184d0b0c6749dd4afb
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-20 16:55:04 +0400

    added homework

---
+++ Homework task 20.04.2022 upperFirstElemet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework_task_20._04._2022_upperFirstElemet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cumle daxil edin: ");
+            string sentence = Console.ReadLine(); 
+            string[] words = sentence.Split(" ");
+            Console.WriteLine("Berpa olunmus cumle: ");
+            foreach (var word in words)
+            { 
+                Console.Write(word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower());
+            }
+            
+           
+          
+
+            
+     
+        }
+    }
+}
